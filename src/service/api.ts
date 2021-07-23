36b95c318a2545a0f1de3678fbba667dd2a1d472
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2021-07-23 15:53:48 -0500

    update

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 axios.defaults.timeout = 10000;
 //https://love-66-api-2-n3sgs.ondigitalocean.app
-const URL = 'http://192.168.100.99:3333';
+const URL = 'https://love-66-api-deewt.ondigitalocean.app';
 const api = axios.create({
   baseURL: URL,
   validateStatus: (status) => {
