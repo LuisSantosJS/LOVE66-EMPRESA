1cc10842e692422cd5466829e065a88633c07246
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2021-08-01 17:10:35 -0500

    update page

--- src/pages/AddEntrega/index.tsx
+++ src/pages/AddEntrega/index.tsx
@@ -81,16 +81,28 @@ interface Features {
 interface OpenEntregas {
     isOpen: boolean | undefined;
 }
+interface DeliverMan {
+    id: string;
+    name_delivermans: "luis santos",
+    email_delivermans: "dasilvasantosluisfelipe@gmail.com",
+    phone_delivermans: "(68) 984198513",
+    vehicle_plate_delivermans: "BRA20E4",
+    vehicle_model_delivermans: "CB1000R",
+}
 const AddEntrega: React.FC<Props> = () => {
     const navigation = useNavigation()
     const [indexDeleteItem, setIndexDeleteItem] = useState<number>(0)
     const [indexUFItem, setIndexUFItem] = useState<number>(0)
+    const [preferenceIdDeliverman, setPreferenceIdDeliverman] = useState<string>('')
     const [indexLocationItem, setIndexLocationItem] = useState<number | null>(null)
     const { userData } = useUserData();
     const [indexPayment, setIndexPayment] = useState<number>(0);
     const { tokenMapBox } = useTokenMapBox()
+    const [deliverman, setDeliverman] = useState<DeliverMan[]>([])
+    const [loadingDeliverman, setLoadingDeliverman] = useState<boolean>(false)
 
     const sheetRef = React.useRef<any>(null);
+    const sheetRefMAN = React.useRef<any>(null);
     const API = axios.create({
         timeout: 5000,
         headers: {
@@ -121,6 +133,22 @@ const AddEntrega: React.FC<Props> = () => {
     const [loading, setLoading] = useState<boolean>(false)
     const [features, setFeatures] = useState<Features[]>([])
 
+
+
+    const loadDeliver = () => {
+        if (loadingDeliverman) return;
+        setLoadingDeliverman(true)
+        sheetRefMAN.current.snapTo(1)
+        api.get('/api/delivermans/disponivels').then((res) => {
+            if (res.data.message === 'success') {
+                setDeliverman(res.data.value)
+            }
+        }).catch((err) => {
+            console.log(err)
+        }).finally(() => {
+            setLoadingDeliverman(false)
+        })
+    }
     const ONPlaces = (index: number) => {
         RNGooglePlaces.openAutocompleteModal()
             .then((place) => {
@@ -363,6 +391,7 @@ const AddEntrega: React.FC<Props> = () => {
         setLoading(true)
         api.post('/api/deliveries', {
             delivermanID: "",
+            preferenceIdDeliverman: preferenceIdDeliverman,
             companiesID: userData.id,
             products: value
         }).then(res => {
@@ -450,6 +479,55 @@ const AddEntrega: React.FC<Props> = () => {
         )
     }
 
+
+
+    const renderContentMAN = () => {
+        //              sheetRefMAN.current.snapTo(0)
+        return (
+            <>
+                <View style={styles.modalss}>
+                    <View style={{ padding: 20 }} />
+
+                    <FlatList
+                        data={deliverman}
+                        refreshing={loadingDeliverman}
+                        onRefresh={loadDeliver}
+                        style={{ flex: 1, width: '100%' }}
+                        ListHeaderComponent={() => {
+                            return (
+                                <Text style={{ color: '#707070', fontSize: 16, textAlign: 'center', width: '100%', paddingLeft:10, paddingRight: 10, paddingBottom: 30}}>*Selecione um entregador para ter uma preferência nesta entrega</Text>
+                            )
+                        }}
+                        renderItem={({ item, index }) => {
+                            return (
+                                <>
+                                    <TouchableOpacity onPress={() => {
+                                        setPreferenceIdDeliverman(item.id)
+                                        sheetRefMAN.current.snapTo(0)
+                                    }} activeOpacity={0.7} style={{ width: '100%', paddingLeft: 30, paddingRight: 30, backgroundColor: '#f7f7f7', alignItems: 'flex-start', justifyContent: 'center', flexDirection: 'column' }}>
+                                        <Text style={{ padding: 3, color: '#707070', fontSize: 16 }}>Nome: {item.name_delivermans}</Text>
+                                        <Text style={{ padding: 3, color: '#707070', fontSize: 16 }}>Modelo da Moto{item.vehicle_model_delivermans}</Text>
+                                        <Text style={{ padding: 3, color: '#707070', fontSize: 16 }}>Placa da Moto{item.vehicle_plate_delivermans}</Text>
+                                    </TouchableOpacity>
+                                </>
+                            )
+                        }}
+                        ItemSeparatorComponent={() => <View style={{ padding: 10 }} />}
+                        ListEmptyComponent={() => {
+                            return (
+                                <View style={{ width: '100%', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+                                    <Text style={{ padding: 10, color: '#707070', fontSize: 16 }}>Não entregadores dispoíveis no momento</Text>
+                                </View>
+                            )
+                        }}
+                        keyExtractor={(item, index) => String(index)}
+                    />
+
+                </View>
+            </>
+        )
+    }
+
     const _renderItem = (item: Entregas, index: number) => {
         return (
             <View style={styles.center}>
@@ -484,9 +562,9 @@ const AddEntrega: React.FC<Props> = () => {
                     <TouchableOpacity
                         activeOpacity={0.8}
                         onPress={() => ONPlaces(index)}
-                        style={[styles.inptusd, {justifyContent:'center'}]}
+                        style={[styles.inptusd, { justifyContent: 'center' }]}
                     >
-                        <Text style={{color:'#707070'}} >{entregas[index]?.address_client || 'Rua, número, cidade...'}</Text>
+                        <Text style={{ color: '#707070' }} >{entregas[index]?.address_client || 'Rua, número, cidade...'}</Text>
                     </TouchableOpacity>
                     {/* </TouchableOpacity> */}
                     <View style={{ padding: 5 }} />
@@ -655,11 +733,16 @@ const AddEntrega: React.FC<Props> = () => {
                                         SOLICITAR ENTREGADOR
                                     </Text>}
                             </TouchableOpacity>
+                            <View style={{ padding: 15 }} />
+                            <TouchableOpacity  onPress={() => preferenceIdDeliverman.length === 0 ? loadDeliver() : setPreferenceIdDeliverman('') } activeOpacity={0.7} >
+                                <Text style={{ color: '#707070', fontSize: 14, padding: 10, textAlign: 'center', textDecorationLine: 'underline' }}>{preferenceIdDeliverman.length === 0 ?  'Dar preferência a um entregador epecífico' : `Remover preferência de ${deliverman[deliverman.findIndex(e => e.id === preferenceIdDeliverman) || 0].name_delivermans}`}</Text>
+                            </TouchableOpacity>
                             <View style={{ padding: 200 }} />
                         </View>
                     </>}
                 />
             </View>
+
 
 
 
@@ -683,6 +766,15 @@ const AddEntrega: React.FC<Props> = () => {
                 borderRadius={30}
                 enabledGestureInteraction={false}
                 renderContent={renderContent}
+            />
+
+
+            <BottomSheet
+                ref={sheetRefMAN}
+                snapPoints={[0, '85%']}
+                borderRadius={30}
+                enabledGestureInteraction={false}
+                renderContent={renderContentMAN}
             />
 
         </>
